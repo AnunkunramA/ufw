727d9dc9623a86c0838bdf727d8fa1aef26592c7
deborah64554 <deborahsgithub@hotmail.com>
2013-05-25 21:24:18 -0700

    Rails up and running
    includes security fixes and CSS structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '7c42b953b8a00f698445fb89957d20cc1d634debfa30cc65737a6458e3e88f8bd3015616823d6f9d919fc79119d4a2be9d967feca746c1e62cb1896a607ef001'
